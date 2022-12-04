37ce65262d480b771c19bb7d2c15c4a2d2011acb
Adil-Rafiq <madilkhan13569@gmail.com>
2022-12-04 06:22:35 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const hamburger = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-nav');
+const mainHeader = document.querySelector('.main_navbar__content');
+
+
+hamburger.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute('data-visible');
+    mainHeader.classList.toggle('expanded');
+
+    if (visibility == "false")
+    {
+        primaryNav.setAttribute('data-visible', true);
+        hamburger.setAttribute('aria-expanded', true);
+    }
+    else {
+        primaryNav.setAttribute('data-visible', false);
+        hamburger.setAttribute('aria-expanded', false);
+    }
+})
+
+/* ------- */
